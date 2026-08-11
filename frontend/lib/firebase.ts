6720ba4148f439fb3cfafbcbd84874efa9059664
Kannayagari Nikhil Sagar <kannayagarinikhil@gmail.com>
2026-08-11 15:11:13 +0530

    chore: secure firebase keys

--- frontend/lib/firebase.ts
+++ frontend/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnEVEBwC0a_iEfAzVXYp36bbyN44bbHNk",
-  authDomain: "zerodhaai.firebaseapp.com",
-  projectId: "zerodhaai",
-  storageBucket: "zerodhaai.firebasestorage.app",
-  messagingSenderId: "569051494429",
-  appId: "1:569051494429:web:a9e0cf5c253f09dd31aef4",
-  measurementId: "G-J8P3R1B5GZ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
